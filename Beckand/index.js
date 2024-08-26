40576607c618b1fdb41f25572f007aaa1ca1fea8
ankit <kumarankitraj1478@gmail.com>
2024-08-26 16:04:52 +0530

    fix

--- Beckand/index.js
+++ Beckand/index.js
@@ -57,7 +57,7 @@ app.post('/convert', upload.single('mp3File'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
